484c2f07a08181c8786740062bf3787057791500
James Ruiz <koolxtreme@gmail.com>
2021-10-26 18:34:08 -0500

    first revision

---
+++ scripts/app.js
@@ -0,0 +1,6 @@
+const slide = document.getElementById("slide");
+const menuBtn = document.getElementById("open");
+const menuBtn1= document.getElementById("close");
+
+menuBtn.addEventListener("click", () => {slide.classList.toggle("show")});
+menuBtn1.addEventListener("click", () => {slide.classList.toggle("show")});
