73f7fd3a2ea85aa0eea5e07e52e0f203842b5a4a
Koolxtreme <koolxtreme@gmail.com>
2022-02-01 14:00:53 -0500

    added links animation

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,29 @@
 const slide = document.getElementById("slide");
 const menuBtn = document.getElementById("open");
 const menuBtn1= document.getElementById("close");
+const parents = document.querySelectorAll('.git__flexbox-div');
+const links = document.querySelectorAll('.grid__link');
 
+links.forEach(link => {
+    link.addEventListener('mouseover', ()=>
+    {
+        let parent = findParent(link);
+        parent.style.background = 'black';
+        parent.style.color = '#fffb'
+    });
+    link.addEventListener('mouseout', ()=>
+    {
+        let parent = findParent(link);
+        parent.style.background = '#fffb';
+        parent.style.color = 'black'
+    })
+});
 menuBtn.addEventListener("click", () => {slide.classList.toggle("show")});
 menuBtn1.addEventListener("click", () => {slide.classList.toggle("show")});
+
+const findParent = (a) => {
+    let parent = a.closest('.git__flexbox-div');
+    return parent
+};
 
 ScrollReveal().reveal('.git__flexbox-div' , {delay:120});
